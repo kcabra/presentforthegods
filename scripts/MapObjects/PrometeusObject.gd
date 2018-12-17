bfcea0dc45420fcbd841228a0dd57783308fce6c
lilak <kcabrak@gmail.com>
2018-12-17 00:15:58 -0300

    implemented promethean objects

--- scripts/MapObjects/PrometeusObject.gd
+++ scripts/MapObjects/PrometeusObject.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 enum Directions {UP_DOWN, LEFT_RIGHT}
 export (Directions) var orientation = Directions.UP_DOWN
-export (int) var float_amount = 100 # how much it moves to each side
+export (int) var float_amount = 5 # how much it moves to each side (in squares
 export (float) var time = 1.0 # how long it takes to go from one edge to the other
 export (bool) var invert = false
 export (float, 0, 1) var offset = 0 # offset in percentage from 0 to 1
@@ -19,13 +19,13 @@ onready var tween = Tween.new()
 func _ready():
 	# tween
 	add_child(tween)
-	connect("tween_completed", tween, "tween_next")
 	
 	# invertion
 	if invert: current = !current
 	
 	# calculate positions
 	init_pos = position
+	float_amount *= 19
 	match orientation:
 		Directions.UP_DOWN:
 			up_edge = init_pos + Vector2(0, -float_amount)
@@ -36,6 +36,15 @@ func _ready():
 			bot_edge = init_pos + Vector2(float_amount, 0)
 	
 	tween_next()
+	stop()
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		is_active = true
+		move()
+	if Input.is_action_just_released("ui_accept"):
+		is_active = false
+		stop()
 
 func tween_next():
 	var initial
@@ -49,16 +58,20 @@ func tween_next():
 			initial = up_edge
 			final = bot_edge
 	
-	position = initial
-	yield(get_tree().create_timer(time), "timeout")
-	position = final
 	current = !current
+	position = initial
+	
+	tween.interpolate_property(self, "position", initial, final, time,
+			Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+	yield(tween, "tween_completed")
+	
 	tween_next()
 
 func move():
 	is_active = true
-	pass
-	
+	tween.resume_all()
+
 func stop():
 	is_active = false
-	pass
+	tween.stop_all()
